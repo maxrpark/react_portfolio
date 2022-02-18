dc9e81f44672278667637c16d0cd396b52915b42
Maxigong <maxi@nolgong.com>
2022-02-18 15:10:30 +0900

    edits

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 import game from '../game.jpeg';
+import styled from 'styled-components';
 // componets
 import Sections from '../components/Sections';
 
@@ -30,12 +31,64 @@ function Home() {
   }, []);
 
   return (
-    <>
+    <Container>
+      <header>
+        <div className='title-center'>
+          <h1>Built with React & Gsap</h1>
+          {/* <a
+            href='https://my-portfolio-blog-website.netlify.app/'
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            Visit my porfolio
+          </a> */}
+        </div>
+      </header>
       {projects.map((project) => {
         return <Sections game={game} key={project.id} {...project} />;
       })}
-    </>
+    </Container>
   );
 }
+const Container = styled.div`
+  .title-center {
+    height: 100vh;
+    width: 100vw;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    color: #fff;
+    background: black;
+  }
+  @media screen and (max-width: 768px) {
+    h1 {
+      text-align: center;
+      width: 200px;
+    }
+  }
+
+  /* a {
+    color: #000;
+    background: white;
+    height: 50px;
+    width: 150px;
+    font-size: 16px;
+    text-transform: capitalize;
+    letter-spacing: normal;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border-radius: 5px;
+    transition: all 0.2s linear;
+    border: 2px solid black;
+    margin-top: 2rem;
+  }
+  a:hover {
+    color: white;
+    background: black;
+    border-color: white;
+  } */
+`;
 
 export default Home;
